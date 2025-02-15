b984f28e6bcbcb0ad3550951a05a8e021a37179a
akshay0444 <akashyap0406@gmail.com>
2025-02-15 17:51:08 +0530

    Add files via upload

---
+++ vdocument.js
@@ -0,0 +1,11 @@
+vdocument.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('proposal').classList.toggle('hidden');
+});
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert("Yay! I'm so happy! ❤️");
+});
+
+document.getElementById('noButton').addEventListener('Run', function() {
+    alert("That's okay! I love you no matter what! 💖");
+});
